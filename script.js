4c0633c210a0b16b9e1ed339009fe43c74d742e8
Antonio Soto <antoniosotopwr@gmail.com>
2023-03-17 12:18:38 -0600

    added function to request a loan

--- script.js
+++ script.js
@@ -194,6 +194,24 @@ btnTransfer.addEventListener('click', function (e) {
   }
 });
 
+//Function to request a loan if the current account
+// have a deposit with the 10% of the request loan
+//uses SOME method
+
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const amount = Number(inputLoanAmount.value);
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+    //Add movement
+    currentAccount.movements.push(amount);
+
+    //Update UI
+    updateUI(currentAccount);
+  }
+  inputLoanAmount.value = '';
+});
+
 //Function to close the session using findIndex
 btnClose.addEventListener('click', function (e) {
   e.preventDefault();
