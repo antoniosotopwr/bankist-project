4b154ccade46b8f3f9c9eeb33cb3c2aee697cfb3
Antonio Soto <antoniosotopwr@gmail.com>
2023-04-25 16:32:32 -0600

    added function to display deposit dates with words instead dd/mm/yyyy format

--- script.js
+++ script.js
@@ -47,9 +47,9 @@ const account1 = {
     '2020-01-28T09:15:04.904Z',
     '2020-04-01T10:17:24.185Z',
     '2020-05-08T14:11:59.604Z',
-    '2020-05-27T17:01:17.194Z',
-    '2020-07-11T23:36:17.929Z',
-    '2020-07-12T10:51:36.790Z',
+    '2023-04-20T17:01:17.194Z',
+    '2023-04-23T23:36:17.929Z',
+    '2023-04-24T10:51:36.790Z',
   ],
   currency: 'EUR',
   locale: 'pt-PT', // de-DE
@@ -103,6 +103,23 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+// FUNCTIONS
+const formatMovementDate = function (date) {
+  const calcDaysPassed = (date1, date2) =>
+    Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
+
+  const daysPassed = calcDaysPassed(new Date(), date);
+  console.log(daysPassed);
+  if (daysPassed === 0) return 'Today';
+  if (daysPassed === 1) return 'Yesterday';
+  if (daysPassed <= 7) return `${daysPassed} days ago`;
+
+  let year = date.getFullYear();
+  let month = `${date.getMonth() + 1}`.padStart(2, '0');
+  let day = `${date.getDate()}`.padStart(2, '0');
+  return `${month}/${day}/${year}`;
+};
+
 //Function to add html content or create dom elements
 const displayMovements = function (acc, sort = false) {
   containerMovements.innerHTML = '';
@@ -116,10 +133,7 @@ const displayMovements = function (acc, sort = false) {
   movs.forEach((mov, i) => {
     //display moveventsDates
     let date = new Date(acc.movementsDates[i]);
-    let year = date.getFullYear();
-    let month = `${date.getMonth() + 1}`.padStart(2, '0');
-    let day = `${date.getDate()}`.padStart(2, '0');
-    let displayDate = `${month}/${day}/${year}`;
+    let displayDate = formatMovementDate(date);
 
     const type = mov > 0 ? 'deposit' : 'withdrawal';
     const html = `
